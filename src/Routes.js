a4bb218b866c82063f1a1a41a5b04ae503324ea3
Alexoid1 <alex_zam@hotmail.es>
2021-01-22 18:23:42 -0500

    math magicians web site

--- src/Routes.js
+++ src/Routes.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from './components/App';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 const Routes = () => (
   <BrowserRouter>
     <Navbar />
     <div>
       <Switch>
-        <Route path="/calculator" component={App} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/calculator" component={App} />
       </Switch>
     </div>
   </BrowserRouter>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,2 +1,35 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+      <div className="paraCont">
+        <h2>Welcome to our Page!</h2>
+
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit.
+          Ac purus in massa egestas mollis varius;
+          dignissim elementum. Mollis tincidunt mattis hendrerit
+          dolor eros enim, nisi ligula ornare.
+          Hendrerit parturient habitant pharetra rutrum gravida
+          porttitor eros feugiat. Mollis elit
+          sodales taciti duis praesent id.
+          Consequat urna vitae morbi nunc congue.
+        </p>
+        <p>
+          Non etiam tempor id arcu magna ante eget.
+          Nec per posuere cubilia cras porttitor condimentum
+          orci suscipit. Leo maecenas in tristique,
+          himenaeos elementum placerat. Taciti rutrum nostra,
+          eget cursus velit ultricies.
+          Quam molestie tellus himenaeos cubilia congue vivamus ultricies.
+          Interdum praesent ut penatibus
+          fames eros ad consectetur sed.
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Home;
